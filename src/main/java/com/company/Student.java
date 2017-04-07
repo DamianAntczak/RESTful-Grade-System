6c2c35d2180c1574721027a84195b88f80b5031c
damiana <d_antczak@op.pl>
2017-04-07 13:10:00 +0200

    Added GET method to students resource

--- src/main/java/com/company/Student.java
+++ src/main/java/com/company/Student.java
@@ -1,16 +1,45 @@
 package com.company;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * Created by Damian on 07.04.2017.
  */
+@XmlRootElement
 public class Student {
     private String indexNr;
     private String name;
     private String surname;
 
+    public Student(){}
+
     public Student(String indexNr, String name, String surname){
         this.indexNr = indexNr;
         this.name = name;
+        this.surname = surname;
+    }
+
+    public String getIndexNr() {
+        return indexNr;
+    }
+
+    public void setIndexNr(String indexNr) {
+        this.indexNr = indexNr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
         this.surname = surname;
     }
 }
--- src/main/java/com/company/StudentsResource.java
+++ src/main/java/com/company/StudentsResource.java
@@ -1,19 +1,36 @@
 package com.company;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Damian on 07.04.2017.
  */
 
 @Path("students")
 public class StudentsResource {
+
+    private ArrayList<Student> studentsList = new ArrayList<Student>();
+
+    public StudentsResource(){
+        this.studentsList.add(new Student("12345","Jan","Nowak"));
+        this.studentsList.add(new Student("12346","Piotr","Kowalski"));
+    }
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Test";
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Student> getAll() {
+        return this.studentsList;
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Student getOne(@PathParam("id") String id){
+        return this.studentsList.stream().filter(s -> s.getIndexNr().equals(id)).findFirst().get();
     }
 }
 
